a055bb0fe4e0d123af685f028e20d76b3e5b9a48
Eddie Kovsky <ewk@edkovsky.org>
2023-05-26 21:00:14 -0600

    Script to clone linux-next
    Never leave to a human what should be automated by a computer.

---
+++ clone-next.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#
+# Clone linux-next on top of Linus' tree
+#
+
+set -e
+
+mkdir -p $HOME/Projects/src
+git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git $HOME/Projects/src/linux
+cd $HOME/Projects/src/linux
+git remote add linux-next git://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git
+git fetch linux-next
+git fetch --tags linux-next
