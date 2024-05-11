0349f89ce00b0cf83d6c52c4a915a6600d824376
Eddie Kovsky <ewk@edkovsky.org>
2024-05-10 20:44:06 -0600

    Quote bash variables

--- clone-next.sh
+++ clone-next.sh
@@ -6,9 +6,9 @@
 
 set -e
 
-mkdir -p $HOME/Projects/src
-git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git $HOME/Projects/src/linux
-cd $HOME/Projects/src/linux
+mkdir -p "$HOME"/Projects/src
+git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git "$HOME"/Projects/src/linux
+cd "$HOME"/Projects/src/linux
 git remote add linux-next git://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git
 git fetch linux-next
 git fetch --tags linux-next
